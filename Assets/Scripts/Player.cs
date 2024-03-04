d57c1d619c83fd27c27f6ae5c0d387a5cacf108b
Kevin Cerro DVRV <kevincerro.dvrv@gmail.com>
2024-03-04 20:31:57 +0100

    Apply last class changes

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,58 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
-{
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour {
     private CharacterController controller;
-    private Animator animator;
     private Vector3 playerVelocity;
     private bool groundedPlayer;
     private float playerSpeed = 2.0f;
     private float jumpHeight = 1.0f;
     private float gravityValue = -9.81f;
 
-    private void Start()
-    {
-        controller = gameObject.GetComponent<CharacterController>();
+    private Animator animator;
+
+    private void Start() {
+        controller = GetComponent<CharacterController>();
         animator = GetComponent<Animator>();
         SetState(PlayerState.Idle);
     }
 
-    void Update()
-    {
+    void Update() {
         groundedPlayer = controller.isGrounded;
+
 
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         move = move.normalized;
         controller.Move(move * Time.deltaTime * playerSpeed);
 
-        if (move != Vector3.zero)
-        {
+        if (move != Vector3.zero) {
             gameObject.transform.forward = move;
             SetState(PlayerState.Run);
         } else {
-            animator.SetBool("running", false);
             SetState(PlayerState.Idle);
         }
 
         // Changes the height position of the player..
-        if (Input.GetButtonDown("Jump") && groundedPlayer)
-        {
-            playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        if (Input.GetButtonDown("Jump") && groundedPlayer)  {
+           playerVelocity.y = Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
+        //Clampeamos la velocidad vertical. 
+        //El máximo está en torno a la velocidad terminal de un humano en caída libre
         playerVelocity.y = Mathf.Clamp(playerVelocity.y, -50f, 50f);
         controller.Move(playerVelocity * Time.deltaTime);
+        
     }
 
-    private void SetState(PlayerState state)
-    {
-        animator.SetTrigger($"{state}");
+    private void SetState(PlayerState newState) {
+        animator.ResetTrigger("Idle");
+        animator.ResetTrigger("Run");
+        animator.SetTrigger($"{newState}");
     }
 }
 
 public enum PlayerState {
     Idle,
-    Run,
+    Run
 }
